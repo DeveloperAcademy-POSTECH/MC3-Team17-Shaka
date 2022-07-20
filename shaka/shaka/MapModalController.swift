e74a9e02fba60ef9fd9ec4a024d9cdd8513c2e4b
Junghoon-P <77766769+Junghoon-P@users.noreply.github.com>
2022-07-20 18:48:31 +0900

    ✨ : MapModalController 추가

---
+++ shaka/shaka/MapModalController.swift
@@ -0,0 +1,16 @@
+//
+//  MapModalController.swift
+//  shaka
+//
+//  Created by JungHoonPark on 2022/07/20.
+//
+
+import UIKit
+
+class MapModalController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+}
