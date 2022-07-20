86a47b9728ea99e8a0727f3ae4cc7d3a8ae37a8c
YunSeok-Choi <dbstjr9898@naver.com>
2022-07-20 22:33:45 +0900

    :sparkles: 앱 로고, 알림, 프로필, 날씨, 서퍼찾기 UI 추가

--- shaka/shaka/ViewController.swift
+++ shaka/shaka/ViewController.swift
@@ -8,9 +8,10 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var weatherButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
     }
 }
