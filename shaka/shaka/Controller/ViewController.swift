31cba1c03caf368c505bba95ea5f6c13531b236c
Junghoon-P <77766769+Junghoon-P@users.noreply.github.com>
2022-07-27 15:39:11 +0900

    ✨ : 메인 레이아웃 구성

--- shaka/shaka/Controller/ViewController.swift
+++ shaka/shaka/Controller/ViewController.swift
@@ -11,6 +11,24 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
+
+    @IBAction func getWaterTapped(_ sender: Any) {
+        guard let url = URL(string: "https://boat.kcg.go.kr/home/wtrlsrActInfo/weathrSpcnwsDclr/infoView1.do") else {
+            return
+        }
+        let webVC = WebViewController(url: url, title: "입수 신고")
+        let navVC = UINavigationController(rootViewController: webVC)
+        present(navVC, animated: true)
+    }
+    
+    @IBAction func offWaterTapped(_ sender: Any) {
+        guard let url = URL(string: "https://boat.kcg.go.kr/home/common/cert_info.do") else {
+            return
+        }
+        let webVC = WebViewController(url: url, title: "퇴수 신고")
+        let navVC = UINavigationController(rootViewController: webVC)
+        present(navVC, animated: true)
+    }
+    
 }
