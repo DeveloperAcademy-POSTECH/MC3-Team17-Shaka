c7b289e2d8b6535c505310487670217b6e9ae37a
Thina <frog7134@gmail.com>
2022-07-28 14:21:25 +0900

    ✨ : SurfRecordView 스토리보드 및 ControllerView 추가

---
+++ shaka/shaka/Controller/SurfRecordViewController.swift
@@ -0,0 +1,15 @@
+//
+//  SurfRecordViewController.swift
+//  shaka
+//
+//  Created by 유정인 on 2022/07/28.
+//
+
+import UIKit
+
+class SurfRecordViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
